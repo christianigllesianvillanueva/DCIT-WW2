61e083aba07c95870046b156b6023f43e09c33df
christianigllesianvillanueva <72721082+christianigllesianvillanueva@users.noreply.github.com>
2020-10-12 11:34:17 +0800

    Create WW2-1(Python).py

---
+++ WW2-1(Python).py
@@ -0,0 +1,12 @@
+num1 = input('Hello, welcome to the sum, difference, product and quotient calculator! Please type the first number. ')
+num2 = input('Please type the second number. ')
+sum = int(num1) + int(num2)
+diff = int(num1) - int(num2)
+quotient = int(num1) / int(num2)
+product = int(num1) * int(num2)
+print('Sum:' + str(sum))
+print('Difference: ' + str(diff))
+print('Product: ' + str(product))
+print('Quotient: ' + str(quotient))
+print('''
+Thanks for using this calculator! ♥-♥  ''')
